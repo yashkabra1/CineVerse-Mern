17e12bc57d838ea98c79094ef7847dd5d50010f1
Yash Kabra <yashkabra930@gmail.com>
2026-07-06 15:17:28 +0530

    Milestone 1: Project setup and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 NovaStream Backend Running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
